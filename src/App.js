f9c6bd2ce1a91c8250c8a5f899eb6c68318f507e
Ann Likowo Molinge <ann.molinge@gmail.com>
2023-07-24 19:35:41 -0500

    Created search feature using form

--- src/App.js
+++ src/App.js
@@ -1,23 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import "./App.css";
+import axios from "axios";
 
 function App() {
+  //Declare state(s)
+  const [word, setWord] = useState({});
+
+  const getWord = async () => {
+    // Use axios to get information from the API. Axios is a library that serves to create HTTP requests that are present externally.
+    const response = await axios.get(
+      "https://api.dictionaryapi.dev/api/v2/entries/en/man"
+    );
+    const infor = await response.data;
+    console.log(infor);
+    setWord(infor);
+  };
+
+  // useEffect to run getWord when component mounts
+  useEffect(() => {
+    getWord();
+  }, []);
+
+  // loaded function for when data is fetched by axios
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>
+        <h1> Word Dictionary </h1>
+      </div>
+      <from action="">
+        <input type="text" placeholder="Seacrh Word" />
+        <input type="submit" />
+      </from>
     </div>
   );
 }
